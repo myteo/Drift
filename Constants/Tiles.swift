efcae48637f3c6e5e82745d0850197e37eaa8093
leon mak <leonmakansheng@gmail.com>
2017-03-09 01:37:50 +0800

    Add basic file structure

---
+++ Constants/Tiles.swift
@@ -0,0 +1,13 @@
+//
+//  Tiles.swift
+//  Drift
+//
+//  Created by Leon on 8/3/17.
+//  Copyright © 2017 nus.cs3217.drift. All rights reserved.
+//
+
+import Foundation
+
+struct Tiles {
+    static let tileMapNodes = ["WaterBg", "GrassBg", "DirtBg"]
+}
---
+++ Drift/GamePlayScene.swift
@@ -0,0 +1,56 @@
+//
+//  GameScene.swift
+//  Drift
+//
+//  Created by Leon on 8/3/17.
+//  Copyright © 2017 nus.cs3217.drift. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    
+    override func sceneDidLoad() {
+
+        self.lastUpdateTime = 0
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        self.lastUpdateTime = currentTime
+    }
+}
