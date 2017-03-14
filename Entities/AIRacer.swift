cf47841e7da4231b735408d3e881f5da0e20afc5
Alex Fong <alexfongg@gmail.com>
2017-03-14 08:40:24 +0800

    Shifted files & renamed bad filenames

---
+++ Entities/AIRacer.swift
@@ -0,0 +1,28 @@
+//
+//  AIRacer.swift
+//  Drift
+//
+//  Created by Teo Ming Yi on 11/3/17.
+//  Copyright © 2017 nus.cs3217.drift. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+class AIRacer: GKEntity {
+
+    init(spriteNode: SKSpriteNode, entityManager: EntityManager) {
+
+        super.init()
+        let spriteComponent = SpriteComponent(entity: self,
+                                              spriteNode: spriteNode)
+        addComponent(spriteComponent)
+        let moveComponent = MoveComponent(type: .AIRacer, node: spriteNode, entityManager: entityManager)
+        addComponent(moveComponent)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
