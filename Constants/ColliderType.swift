b46b86ba86b6523d1cee1ad00c6141dc2e41a141
Alex Fong <alexfongg@gmail.com>
2017-03-16 22:22:44 +0800

    Collisions are now properly detected

--- Constants/ColliderType.swift
+++ Constants/ColliderType.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct ColliderType {
-    static let Vehicles: UInt32 = 1 >> 0
-    static let Obstacles: UInt32 = 1 >> 0
-    static let PowerUp: UInt32 = 1 >> 2
+    static let Vehicles: UInt32 = 1 << 0
+    static let Obstacles: UInt32 = 1 << 1
+    static let PowerUp: UInt32 = 1 << 2
 }
